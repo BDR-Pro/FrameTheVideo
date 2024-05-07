36ece5deff26378803d21a185398174035ae1353
Bader alotaibi <baderalotaibi3@gmail.com>
2024-05-07 03:21:00 +0300

    mega

--- FrameTheVideo/web/views.py
+++ FrameTheVideo/web/views.py
@@ -23,55 +23,27 @@ import threading
 import re
 from django.http import JsonResponse
 
+from mega import Mega
+
 
 Password = settings.EMAIL_HOST_PASSWORD
 
-from google.oauth2.service_account import Credentials
-from googleapiclient.discovery import build
-from googleapiclient.http import MediaFileUpload
-import os
 
-def authenticate_using_service_account():
-    """Authenticate to Google API using a service account JSON key file."""
-    try:
-        scopes = ['https://www.googleapis.com/auth/drive']
-        json_key_file_path = os.path.join(os.path.dirname(__file__), "django-422416-eda7f29f423d.json")
-        credentials = Credentials.from_service_account_file(json_key_file_path, scopes=scopes)
-        return build('drive', 'v3', credentials=credentials)
-    except Exception as e:
-        logging.error(f"Failed to authenticate using service account: {e}")
-        print(f"Failed to authenticate using service account: {e}")
-        return None
+def upload_file_to_mega(file_path):
+    # Initialize Mega object
+    mega = Mega()
 
-def upload_to_google_drive(file_path):
-    """Upload a file to Google Drive using a service account and return the shareable link.
+    # Login to your Mega.nz account
+    m = mega.login('framethevideo@gmail.com', Password)
 
-    Args:
-        file_path (str): The path to the file to upload.
+    # Upload a file
+    file = m.upload(file_path)
 
-    Returns:
-        str: The shareable link to the uploaded file.
-    """
-    try:
-        file_metadata = {
-            'name': os.path.basename(file_path),
-            'mimeType': 'application/octet-stream'
-        }
-        service = authenticate_using_service_account()
-        media = MediaFileUpload(file_path, mimetype='application/octet-stream', resumable=True)
-        file = service.files().create(body=file_metadata, media_body=media, fields='id, webViewLink').execute()
-        
-        # Make the file shareable
-        service.permissions().create(
-            fileId=file['id'],
-            body={'type': 'anyone', 'role': 'reader'},
-            fields='id'
-        ).execute()
-        print(f"Uploaded file to Google Drive: {file['webViewLink']}")
-        return file['webViewLink']
-    except Exception as e:
-        print(f"Failed to upload file to Google Drive: {e}")
-        return None
+    # Get the public link to the uploaded file
+    public_link = m.get_upload_link(file)
+    print("File uploaded. Public link: ", public_link)
+    return public_link
+
 
 
 def main(request):
@@ -149,7 +121,7 @@ def queue(video_id, output_folder,email):
         sleep(60*60*1)  # 1 hour
         zip_file_path = os.path.join(output_folder, f"{(video_id)}_frames.zip")
                 
-        return send_email(email, zip_file_path)
+        return send_email(email, zip_file_path, yt_to_title(video_id))
     except Exception as e:
         logging.error(f"Failed to process video (q): {e}")
 
@@ -188,7 +160,7 @@ def store_in_text(email, zip_file_path):
         
 
 
-def send_email(email, zip_file_path):
+def send_email(email, zip_file_path, title):
     """Send an email with the zip file attached.
     
     Args:
@@ -209,13 +181,13 @@ def send_email(email, zip_file_path):
         # Set the body of the email
         body = MIMEText("Hello, \n\nThank you for using FrameTheVideo! \n\nPlease find the attached zip file containing the wallpaper images extracted from the video you requested. \n\nEnjoy! \n\nBest regards, \nBader Alotaibi (BDR-PRO) Github", 'plain')
         msg.attach(body)
-        name_of_zip = os.path.basename(zip_file_path).replace(".zip", "")
-        msg.attach(MIMEText(f"\n For the video: {name_of_zip}", 'plain'))
+        
+        msg.attach(MIMEText(f"\n\nFor the video: {title}", 'plain'))
         # upload the zip file to google drive
-        link = upload_to_google_drive(zip_file_path)
-        logging.info(f"Uploaded zip file to Google Drive: {link}")
-        print(f"Uploaded zip file to Google Drive: {link}")
-        msg.attach(MIMEText(f"\n Download the zip file from Google Drive: {link}", 'plain'))
+        link = upload_file_to_mega(zip_file_path)
+        logging.info(f"Uploaded zip file to Mega: {link}")
+        print(f"Uploaded zip file to Mega: {link}")
+        msg.attach(MIMEText(f"\nDownload the zip file from Google Drive: {link}", 'plain'))
         
         # Set up the SMTP server and send the email
         with smtplib.SMTP('smtp.gmail.com', 587) as server:
